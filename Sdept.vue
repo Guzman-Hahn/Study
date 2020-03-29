57705346bad0954ecf200d9a0d5b8192bff7a2e4
Guzman-Hahn <52264615+Guzman-Hahn@users.noreply.github.com>
2020-03-30 02:12:34 +0800

    Add files via upload

---
+++ Sdept.vue
@@ -0,0 +1,374 @@
+<template>
+	<div class="wrap">
+		<el-row>
+			<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"><div class="inner"></div><img src="./assets/img/top.jpg" alt="" width="100%" ></el-col>
+			  
+		</el-row>
+		<el-row>
+			<el-col :span="18" :push="3"><div class="content">
+					<el-row class="header">
+						<el-col :span="4"><span>学部概况</span></el-col>
+						<el-col :span="4"><span>教室队伍</span></el-col>
+						<el-col :span="4"><span>学部动态</span></el-col>
+						<el-col :span="4"><span>学术期刊</span></el-col>
+						<el-col :span="4"><span>学者观点</span></el-col>
+						<el-col :span="4"><span>经管学部大讲堂</span></el-col>
+					</el-row>
+					<el-row>
+						<el-col :span="24"><div class="rotation"></div></el-col>
+					</el-row>
+					<el-row>
+						<el-col :span="24">
+							<div class="prev-top">
+								<el-row>
+									<el-col :xs="24" :sm="24" :md="13" :lg="13" :xl="13">
+										<div class="left">
+											<div class="tag">
+												<span>学部动态</span>
+												<span>更多>></span>
+											</div>
+											<div class="leab">
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+											</div>
+										</div>
+									</el-col>
+									<el-col :xs="24" :sm="24" :md="13" :lg="10" :xl="10" :push="1">
+										<div class="right">
+											<div class="tag">
+												<span>学术期刊</span>
+												<span>更多>></span>
+											</div>
+											<div class="leab">
+												<p class="leab-text">
+													<span>经济科学</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>组织管理研究</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>组织管理研究</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>组织管理研究</span>
+													<span>02-17</span>
+												</p>
+												
+											</div>
+									    </div>
+									</el-col>
+								</el-row>
+							</div>
+						</el-col>
+					</el-row>
+					<el-row>
+						<el-col :span="24">
+							<div class="prev-buttom">
+								<el-row>
+									<el-col :xs="24" :sm="24" :md="13" :lg="13" :xl="13">
+										<div class="left">
+											<div class="tag">
+												<span>经管学部大讲堂</span>
+												<span>更多>></span>
+											</div>
+											<div class="leab">
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+												<p class="leab-text">
+													<span>经济与管理学部相关院系部分在线课程资料</span>
+													<span>02-17</span>
+												</p>
+											</div>
+										</div>
+								    </el-col>
+									<el-col :xs="24" :sm="24" :md="13" :lg="10" :xl="10" :push="1">
+										<div class="right">
+											<div class="sorter-top">
+												<span>院系所中心</span>
+												<label>相关链接</label>
+											</div>
+											<div class="sorter-btn">
+												<ul class="sorter-lt">
+													<li>经济学院</li>
+													<li>光华管理学院</li>
+													<li>国家发展研究院</li>
+													<li>人口研究所</li>
+													
+												</ul>
+												<ul class="sorter-rg">
+													<li>学科建设办公室</li>
+													<li>理学部</li>
+													<li>信息与工程科学部</li>
+													<li>社会科学学部</li>
+													<li>人文学学部</li>
+												</ul>
+											</div>
+										</div>
+									</el-col>
+								</el-row>
+							</div>
+						</el-col>
+					</el-row>
+			    </div>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="24">
+				<div class="bottom">
+					<div class="bottom-img">
+						<img src="./assets/img/logob1.png" width="100%" style="height: 30%;">
+					</div>
+					<div class="massage">
+						<p>重庆经贸职业技术学院版权所有2011-1017</p>
+						<p>学校地址：重庆市黔江武陵大道北段1785号</p>
+						<p>Copyright 2014 jinmaixueyuan AllRight Reseved</p>
+						<p>技术支持：老鱼哥网络科技</p>
+					</div>
+					<div class="phone">
+						<p>招生电话：023-79320888-7931888</p>
+						<p>网络备案：渝ICP备19010118</p>
+					</div>
+					<div class="code">
+						<div>
+							<img src="./assets/img/blog.png" alt="">
+							<p>官方微博</p>
+						</div>
+						<div>
+							<img src="./assets/img/wechat.png" alt="">
+							<p>官方微信</p>
+						</div>
+					</div>
+				</div>
+			</el-col>
+			  
+		</el-row>
+		
+	</div>
+</template>
+<script>
+	
+</script>
+<style lang="less">
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, th, td { margin:0; padding:0; }
+body, button, input, select, textarea { font:12px/1.5tahoma, arial, \5b8b\4f53; }
+h1, h2, h3, h4, h5, h6{ font-size:100%; }
+address, cite, dfn, em, var { font-style:normal; }
+code, kbd, pre, samp { font-family:couriernew, courier, monospace; }
+small{ font-size:12px; }
+ul, ol { list-style:none; }
+a { text-decoration:none; }
+a:hover { text-decoration:underline; }
+sup { vertical-align:text-top; }
+sub{ vertical-align:text-bottom; }
+legend { color:#000; }
+fieldset, img { border:0; }
+button, input, select, textarea { font-size:100%; }
+table { border-collapse:collapse; border-spacing:0; }
+.wrap{
+	max-width: 1920px;
+	.content{
+		min-height: 1066px;
+		.header{
+			min-height: 55px;
+			text-align: center;
+			
+			span{
+				line-height: 55px;
+				font-size: 20px;
+			}
+		}
+		.rotation{
+			min-height: 315px;
+			background: url(assets/img/banner-news.jpg) !important;
+			margin-bottom: 46px;
+			
+		}
+		.tag{
+			min-height:46px ;
+			width: 100%;
+			line-height: 46px;
+			border-bottom:3px solid #0860af;
+			margin-bottom: 3px;
+			
+			span:nth-of-type(1){
+				font-size: 22px;
+				
+			}
+			span:nth-of-type(2){
+				font-size: 15px;
+				color: #a9aaab;
+				float: right;
+			}
+		}
+		.leab{
+			width: 100%;
+			margin-left: 8px;
+			.leab-text{
+				margin-top: 19px;
+				span:nth-of-type(1){
+					font-size: 20px;
+					
+				}
+				span:nth-of-type(2){
+					font-size: 18px;
+					color: #0f63af;
+					float: right;
+				}
+				strong{
+					font-size: 20px;
+					margin-left: 20%;
+				}
+			}
+		}
+		.prev-top{
+			.left{
+				min-height: 315px;
+				
+				
+			}
+			.right{
+				
+				min-height: 315px;
+			}
+		}
+		.prev-buttom{
+			margin: 45px 0;
+			.right{
+				min-height: 315px;
+				.sorter-top{
+					min-height:46px ;
+					width: 100%;
+					line-height: 46px;
+					border-bottom:3px solid #0860af;
+					margin-bottom: 3px;
+					span{
+						font-size: 22px;
+					}
+					label{
+						font-size: 22px;
+						margin-left: 29%;
+					}
+				}
+				.sorter-btn{
+					width: 100%;
+					
+					.sorter-lt{
+						li{
+							margin-left: 8px;
+							margin-top: 19px;
+							font-size: 20px;
+						}
+						width: 50%;
+						height: 264px;
+						float: left;
+						margin-top: -20px;
+					}
+					.sorter-rg{
+						li{
+							margin-top: 19px;
+							font-size: 20px;
+						}
+					}
+				}
+			}
+			.left{
+				min-height: 315px;
+			}
+		}
+	}
+	
+
+		.bottom{
+			height: 336px;
+			width: 100%;
+			padding:2% 8.2% 2% 8.2%;
+			box-sizing: border-box;
+			background: #3d5890;
+			position:relative;
+			.bottom-img{
+				img{
+					max-width:300px;
+					max-height:90px
+				}
+				
+			}
+			p{
+				color: #bcc3d6;
+				font-size: 15px;
+				line-height: 36px;
+			}
+			.massage{
+				position: absolute;
+				left: 8.5%;
+				top: 40%;
+			}
+			.phone{
+				position: absolute;
+				left: 40%;
+				top: 40%;
+			}
+			.code{
+				width: 25%;
+				position: absolute;
+				left: 68%;
+				top: 30%;
+				&>div{
+					width: 25%;
+					height: 100%;
+					float: left;
+					p{
+						text-align: center;
+					}
+				}
+				&>div:nth-of-type(1){
+					margin-right:25%;
+				}
+			}
+		
+	}
+}
+
+</style>
